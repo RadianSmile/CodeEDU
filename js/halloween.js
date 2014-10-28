8bc7958ac688274d5f110eff10ef0727f90b559c
s914211 <cchccy1623@gmail.com>
2014-10-29 01:18:34 +0800

    for halloween debug

--- js/halloween.js
+++ js/halloween.js
@@ -1,6 +1,7 @@
 Parse.initialize("9eo5r1mHWoIPSTCzmrpdKa3lcHPjySx4y5D6q8Nq", "R8SWwYxpJcy73ogQKuSD43y7FigrlDGjBLcy1lzC");
 
 function halloween(){
+  var count = 0;
   var userstatus = Parse.Object.extend('User_status');
   var query = new Parse.Query(userstatus);
   query.descending("XP");
@@ -8,24 +9,24 @@ function halloween(){
   query.find({
     success:function(alluser){
       for(var i = 0; i<alluser.length; i++){
-            var count = 0;
+            var userobject = alluser[i];
             var userrole = alluser[i].get('User').get('role');
             if(userrole == "student"){
+              count++;
               if(count <= 30){
                 var cardinfo = Parse.Object.extend('Card_info');
                 var query = new Parse.Query(cardinfo);
                 query.equalTo('objectId', "1PF6Z8XISA");
                 query.first({
                   success:function(stealcard){
-                    console.log(alluser[i]);
+                    console.log(userobject);
                     var Owncard = Parse.Object.extend('Owncard');
                     var owncard = new Owncard();
-                    owncard.set('user', alluser[i]);
+                    owncard.set('user', userobject);
                     owncard.set('Card_info', stealcard);
                     owncard.save(null, {
                       success:function(){
                         console.log("發放偷竊卡片卡成功!");
-                        count++;
                       },
                       error:function(error){
                         console.log(error);
