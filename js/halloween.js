6cc4ae4a9914ef3d4f87b5d7a6ca5ad2cd6a75f3
Tommy <cchccy1623@gmail.com>
2014-10-29 18:26:46 +0800

    for debug

--- js/halloween.js
+++ js/halloween.js
@@ -11,6 +11,87 @@ function halloween(){
       for(var i = 0; i<alluser.length; i++){
             var userrole = alluser[i].get('User').get('role');
             if(userrole == "student"){
+                var userobject = alluser[i].id;
+                localStorage.setItem('userobject', userobject);
+                count++;
+                if(count <= 27){
+                  var user = Parse.Object.extend('User');
+                  var userquery = new Parse.Query(user);
+                  var retrievedObject = localStorage.getItem('userobject');
+                  userquery.equalTo('objectId', retrievedObject);
+                  userquery.first({
+                    success:function(user){
+                        var cardinfo = Parse.Object.extend('Card_info');
+                        var query = new Parse.Query(cardinfo);
+                        query.get("1PF6Z8XISA",{
+                            success:function(stealcard){
+                                var Owncard = Parse.Object.extend('Owncard');
+                                var owncard = new Owncard();
+                                owncard.set('user', user);
+                                owncard.set('Card_info', stealcard);
+                                owncard.save(null, {
+                                    success:function(){
+                                        console.log("發放偷竊卡片卡成功!");
+                                        localStorage.removeItem('userobject');
+                                    },
+                                    error:function(error){
+                                        console.log(error);
+                                    }
+                                })
+                            },
+                            error:function(error){
+                                console.log(error);
+                            }
+                        })
+                    }
+                  })
+                }
+                else{
+                  var user = Parse.Object.extend('User');
+                  var userquery = new Parse.Query(user);
+                  var retrievedObject = localStorage.getItem('userobject');
+                  userquery.equalTo('objectId', retrievedObject);
+                  userquery.first({
+                    success:function(user){
+                        var cardinfo = Parse.Object.extend('Card_info');
+                        var query = new Parse.Query(cardinfo);
+                        query.get("aJONHaxQtM",{
+                            success:function(lifecard){
+                                var Owncard = Parse.Object.extend('Owncard');
+                                var owncard = new Owncard();
+                                owncard.set('user', user);
+                                owncard.set('Card_info', lifecard);
+                                owncard.save(null, {
+                                    success:function(){
+                                        console.log("發放加生命值卡成功!");
+                                        localStorage.removeItem('userobject');
+                                    },
+                                    error:function(error){
+                                        console.log(error);
+                                    }
+                                })
+                            }
+                        })
+                    }
+                  })
+                }
+            } 
+      }
+    }
+  })
+}
+
+/*function halloween(){
+  var count = 0;
+  var userstatus = Parse.Object.extend('User_status');
+  var query = new Parse.Query(userstatus);
+  query.descending("XP");
+  query.include('User');
+  query.find({
+    success:function(alluser){
+      for(var i = 0; i<alluser.length; i++){
+            var userrole = alluser[i].get('User').get('role');
+            if(userrole == "student"){
                 var userobject = alluser[i];
                 localStorage.setItem('userobject', userobject);
                 count++;
@@ -68,4 +149,4 @@ function halloween(){
       }
     }
   })
-}
+}*/
